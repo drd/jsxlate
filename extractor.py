2f2cc4bdb36f18abd1ef7c71b2a803da28cb2254
David McCabe <dave.mccabe@idealist.org>
2014-10-06 17:15:44 -0700

    Ability to descend through all ASTs, not just expressions

--- extractor.py
+++ extractor.py
@@ -33,12 +33,9 @@ def tree_by_adding_materialized_expression_to_tree(expr, tree, parent_index=None
     nodes, adj = tree
     my_index = len(nodes)
     if parent_index is not None: adj.append((parent_index, my_index))
-    if is_interesting(expr):
-        nodes.append(without_children(expr))
-        for child in immediate_subexpressions(expr):
-            tree = tree_by_adding_materialized_expression_to_tree(child, tree, my_index)
-    else:
-        nodes.append(expr)
+    nodes.append(without_children(expr))
+    for child in immediate_subexpressions(expr):
+        tree = tree_by_adding_materialized_expression_to_tree(child, tree, my_index)
     return tree
 
 def materialized_expression_from_tree(tree):
@@ -202,11 +199,154 @@ class Struct(object):
     def __init__(self, entries):
         self.__dict__.update(entries)
 
+# def immediate_subexpressions(expression):
+#     if expression['type'] in ('NewExpression', 'CallExpression'):
+#         return expression['arguments'][1:]
+#     else:
+#         return []
+
+
+def singles(struct, *names):
+    return [(name, struct['name'])
+        for name in names
+        if struct['name']]
+
+def multi(struct, name):
+    return [(name, x) for x in struct['name']]
+
+
+
+{
+    'Program':             ('', 'body'),
+    'Function':            ('defaults', 'body'),
+    'FunctionDeclaration': ('defaults', 'body'),
+    'FunctionExprssion':   ('defaults', 'body'),
+    'ArrowExpression':     ('defaults', 'body'),
+    'ExpressionStatement': ('expression', ''),
+    'IfStatement':         ('test consequent alternate', ''),
+    'LabeledStatement':    ('body', ''),
+    'WithStatement':       ('object body', ''),
+    'SwitchStatement':     ('discriminant', 'cases'),
+    'ReturnStatement':     ('argument', ''),
+    'ThrowStatement':      ('argument', ''),
+    'TryStatement':        ('block handler finalizer', 'guardedHandlers'),
+    'WhileStatement':      ('test body', ''),
+    'DoWhileStatement':    ('test body', ''),
+    'ForStatement':        ('init test update body', ''),
+    'ForInStatement':      ('left right body', ''),
+    'ForOfStatement':      ('left right body', ''),
+    'LetStatement':        ('body', 'head'),
+    'LetExpression':       ('body', 'head'),
+    'VariableDeclaration': ('', 'declarations'),
+    'VariableDeclarator':  ('init', ''),
+    'ArrayExpression':     ('', 'elements'),
+    'ObjectExpression':    ('', 'properties'),
+    'Property':            ('key value', ''),
+    'SequenceExpression':  ('', 'expressions'),
+    'UnaryExpression':     ('argument', ''),
+    'UpdateExpression':    ('argument', ''),
+    'YieldExpression':     ('argument', ''),
+    'AssignmentExpression': ('left right', ''),
+    'BinaryExpression':    ('left right', ''),
+    'LogicalExpression':   ('left right', ''),
+    'ConditionalExpression': ('test alternate consequent', ''),
+    'NewExpression':       ('callee', 'arguments'),
+    'CallExpression':      ('callee', 'arguments'),
+    'MemberExpression':    ('object property'),
+    'ComprehensionExpression': ('body filter', 'blocks'),
+    'GeneratorExpression': ('body filter', 'blocks'),
+    'SwitchCase':          ('test', 'consequent'),
+    'CatchClause':         ('guard body', ''),
+    'ComprehensionBlock':  ('', 'right'),
+}
+
+def ast_children(ast_node):
+    type = n['type']
+    if type in ('Program', 'BlockStatement'):
+        return multi(n, 'body')
+    if type in ('Function', 'FunctionDeclaration', 'FunctionExprssion', 'ArrowExpression'):
+        singles(n,'defaults') + multi(n, 'body')
+    if type == 'ExpressionStatement':
+        return singles(n, 'expression')
+    if type == 'IfStatement':
+        return singles(n, 'test', 'consequent', 'alternate')
+    if type == 'LabeledStatement':
+        return singles(n, 'body')
+    if type == 'WithStatement':
+        return singles(n, 'object', 'body')
+    if type == 'SwitchStatement':
+        return singles(n, 'discriminant') + multi(n, 'cases')
+    if type in ('ReturnStatement', 'ThrowStatement'):
+        return singles(n, 'argument')
+    if type == 'TryStatement':
+        return singles(n, 'block', 'handler', 'finalizer') + multi(n, guardedHandlers)
+    if type in ('WhileStatement', 'DoWhileStatement'):
+        return singles(n, 'test', 'body')
+    if type == 'ForStatement':
+        return singles(n, 'init', 'test', 'update', 'body')
+    if type in ('ForInStatement', 'ForOfStatement'):
+        return singles(n, 'left', 'right', 'body')
+    if type in ('LetStatement', 'LetExpression'):
+        return singles(n, 'body') + multi(n, 'head')
+    if type == 'VariableDeclaration':
+        return multi(n, 'declarations')
+    if type == 'VariableDeclarator':
+        return singles(n, 'init')
+    if type == 'ArrayExpression':
+        return multi(n, 'elements')
+    if type == 'ObjectExpression':
+        return multi(n, 'properties')
+    if type == 'Property':
+        return singles(n, 'key', 'value')
+    if type == 'SequenceExpression':
+        return multi(n, 'expressions')
+    if type in ('UnaryExpression', 'UpdateExpression', 'YieldExpression'):
+        return singles(n, 'argument')
+    if type in ('AssignmentExpression', 'BinaryExpression', 'LogicalExpression'):
+        return singles(n, 'left', 'right')
+    if type == 'ConditionalExpression':
+        return singles(n, 'test', 'alternate', 'consequent')
+    if type in ('NewExpression', 'CallExpression'):
+        return singles(n, 'callee') + multi(n, 'arguments')
+    if type == 'MemberExpression':
+        return singles(n, 'object', 'property')
+    if type in ('ComprehensionExpression', 'GeneratorExpression'):
+        return singles(n, 'body', 'filter') + multi(n, 'blocks')
+    if type == 'SwitchCase':
+        return singles(n, 'test') + multi(n, 'consequent')
+    if type == 'CatchClause':
+        return singles(n, 'guard', 'body')
+    if type == 'ComprehensionBlock':
+        return singles(n, 'right')
+
+
 def immediate_subexpressions(expression):
-    if expression['type'] in ('NewExpression', 'CallExpression'):
-        return expression['arguments'][1:]
-    else:
+    e = Struct(expression)
+    type = e.type
+    if type in ('ThisExpression', 'FunctionExprssion', 'ArrowExpression', 'GraphExpression', 'GraphIndexExpression', 'Literal', 'Identifier'):
         return []
+    elif type == 'ArrayExpression':
+        return e.elements
+    elif type == 'ObjectExpression':
+        return [property['value'] for property in e.properties]
+    elif type == 'SequenceExpression':
+        return e.expressions
+    elif type in ('UnaryExpression', 'UpdateExpression'):
+        return [e.argument]
+    elif type in ('BinaryExpression', 'AssignmentExpression', 'LogicalExpression'):
+        return [e.left, e.right]
+    elif type == 'ConditionalExpression':
+        return [e.test, e.alternate, e.consequent]
+    elif type in ('NewExpression', 'CallExpression'):
+        return expression['arguments'][1:] # XXX
+    elif type == 'MemberExpression':
+        return [e.object, e.property] if e.computed else [e.object]
+    elif type == 'YieldExpression':
+        return [e.argument] if e.argument else []
+    elif type in ('ComprehensionExpression', 'GeneratorExpression'):
+        return [e.body] + [block.right for block in e.blocks] + ([e.filter] if e.filter else [])
+    elif type == 'LetExpression':
+        return sum([[head.init] if head.init else [] for head in e.head], [e.body])
 
 
 # INTERESTINGNESS
@@ -350,20 +490,41 @@ def extract(fileobj, keywords, comment_tags, options):
             '')
 
 
+def message_from_node_in_tree(node, tree):
+    expr = expression_by_materializing_expression_with_tree(node, tree)
+    return babel_message_for_expression(understand(expr)[0])
+
+def extract_messages_from_tree(tree, index=0):
+    node = tree.nodes[index]
+    if is_string_literal(node) or is_dom_node(node):
+        yield message_from_node_in_tree(node, tree)
+    else:
+        for p, c in tree.adjacency:
+            if p == index:
+                for x in extract_messages_from_tree(tree, c):
+                    yield x
+
 
 if __name__ == '__main__':
     program = parse_file('test.js')
-    destructively_assign_parent_types_to_ast(program)
-    expressions = list(top_level_expressions_in_ast(program))
-    trees = [tree_from_materialized_expression(e) for e in expressions]
-    roots = map(root, trees)
-    # pprint([generate(r) for r in roots])
-    # print len(roots)
-    # print "----"
-    pprint([generate(materialized_expression_from_tree(t)) for t in trees])
+    print ast_children(program)
+    # destructively_assign_parent_types_to_ast(program)
+    # tree = tree_from_materialized_expression(program)
+    # assert materialized_expression_from_tree(tree) == program
+    # print tree.nodes
 
-    for e in expressions:
-        assert e == materialized_expression_from_tree(tree_from_materialized_expression(e))
+
+    # expressions = list(top_level_expressions_in_ast(program))
+    # trees = [tree_from_materialized_expression(e) for e in expressions]
+    # roots = map(root, trees)
+    # # pprint([generate(r) for r in roots])
+    # # print len(roots)
+    # # print "----"
+    # pprint([generate(materialized_expression_from_tree(t)) for t in trees])
+    # pprint([list(extract_messages_from_tree(t)) for t in trees])
+
+    # for e in expressions:
+    #     assert e == materialized_expression_from_tree(tree_from_materialized_expression(e))
 
 
     # pprint(set(x['value'] for x in string_literals_in_ast(program)))
