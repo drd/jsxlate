0537bcd6d5ef63d04bfaf5cb3f79174c84ac1cf9
Eric O'Connell <eric.oconnell@idealist.org>
2015-09-23 11:23:05 -0700

    Adds transform plugin

---
+++ src/transform.js
@@ -0,0 +1,54 @@
+const babel = require('babel');
+
+const ast = require('./ast');
+const extraction = require('./extraction');
+const freeVariables = require('./free-variables');
+
+
+module.exports = function({Plugin, types: t}) {
+    return new Plugin('transformation', {
+        visitor: {
+            JSXElement: function(node, parent) {
+                if (ast.isElementMarker(node)) {
+                    const vars = freeVariables.freeVariablesInMessage(node);
+                    const message = extraction.extractElementMessage(node);
+                    return t.callExpression(
+                        t.memberExpression(
+                            t.identifier('React'),
+                            t.identifier('createElement')
+                        ),
+                        [
+                            t.literal('I18N'),
+                            t.objectExpression([
+                                t.property('init', t.identifier('message'), t.literal(message)),
+                                t.property('init', t.identifier('context'), t.arrayExpression(
+                                    vars.map(v => t.identifier(v))
+                                )),
+                                t.property('init', t.identifier('fallback'), t.functionExpression(
+                                    null,
+                                    [],
+                                    t.blockStatement([
+                                        t.returnStatement(
+                                            t.callExpression(
+                                                t.memberExpression(
+                                                    t.identifier('React'),
+                                                    t.identifier('createElement')
+                                                ),
+                                                [
+                                                    t.literal('span'),
+                                                    t.identifier('null'),
+                                                    t.arrayExpression(node.children)
+                                                ]
+                                            )
+                                        )
+                                    ])
+                                ))
+                            ]),
+                            t.identifier('null')
+                        ]
+                    )
+                }
+            }
+        }
+    });
+};
