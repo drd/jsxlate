1cdb2c5a71f26d782bdf2fad3117c375a1480969
Eric O'Connell <eric.oconnell@idealist.org>
2016-03-02 10:20:13 -0800

    Only compile javascript in build

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 echo "* Building jsxlate"
 mkdir -p ./lib
 babel=$(npm bin)/babel
-for f in $(ls src); do $babel src/$f > build/$f; done
+for f in $(ls src/*.js*); do $babel src/$f > build/$f; done
 echo "* Built."
