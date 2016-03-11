4704664724f11701c5e26c226113b49d9bb4f919
Eric O'Connell <eric.oconnell@idealist.org>
2016-03-11 13:00:46 -0800

    Return of jsxlate-lint

--- bin/jsxlate-lint.js
+++ bin/jsxlate-lint.js
@@ -4,11 +4,11 @@
 var chalk = require('chalk');
 var fs = require('fs');
 
-var jsxlate = require('../lib/jsxlate');
 var filesFromMixedPaths = require('./filesFromMixedPaths');
+var findUntranslatedStrings = require('../build/lint').default;
 
 function showHelpAndExit() {
-    console.log("Usage: i18n-lint ...FILES/DIRECTORIES");
+    console.log("Usage: jsxlate-lint ...FILES/DIRECTORIES");
     console.log("Looks through FILES/DIRECTORIES for strings which should be wrapped in <I18N> tags.");
     process.exit();
 }
@@ -23,21 +23,20 @@ if (argv._.length === 0 || argv.h) {
 }
 
 var paths = filesFromMixedPaths(argv._, {ignore: argv.I});
-
 paths.forEach(function(path) {
     try {
         var src = fs.readFileSync(path, "utf8");
-        var suspiciousStrings = jsxlate.findUntranslatedStrings(src);
-        if (suspiciousStrings.size) {
+        var suspiciousStrings = findUntranslatedStrings(src);
+        if (suspiciousStrings.length) {
             console.log(chalk.bold.yellow(path + ':'));
             console.log(suspiciousStrings.map(function(node) {
-                var node = node.toJS();
-                return node.loc.start.line + ": " + node.raw;
-            }).toJS().join('\n'));
+                return node.loc.start.line + ": " + node.value;
+            }).join('\n'));
         }
     } catch(e) {
         console.error(chalk.bold.red("\nError in file " + path + ":"));
-        console.error(jsxlate.errorMessageForError(e));
+        console.error(e);
+        console.error(e.stack);
         process.exit(1);
     }
 
