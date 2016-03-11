9b1ef95887e821da8be2a11e599c804124fd88c7
stevenp <steven.peterson@gmail.com>
2016-03-11 11:24:15 -0600

    Add support for output format

--- bin/extract-messages.js
+++ bin/extract-messages.js
@@ -4,15 +4,16 @@
 require('babel-polyfill');
 
 function showHelpAndExit() {
-    console.log("Usage: extract-messages [-m EXISTING] [-o OUTPUT] ...FILES/DIRECTORIES");
-    console.log("Prints a JSON document with messages in FILES/DIRECTORIES mapped to themselves.");
+    console.log("Usage: extract-messages [-m EXISTING] [-o OUTPUT] [-f FORMAT (po|json)]...FILES/DIRECTORIES");
+    console.log("Prints a document (gettext PO format by default) with messages in")
+    console.log("FILES/DIRECTORIES mapped to themselves.");
     console.log("If -o is passed, writes to OUTPUT instead of stdout.");
     process.exit();
 }
 
 var argv = require('minimist')(process.argv.slice(2), {
-    string: 'oh',
-    alias: {o: 'output', h: 'help'}
+    string: ['ohf'],
+    alias: {o: 'output', h: 'help', f: 'format'}
 });
 
 if (argv._.length === 0 || argv.h) {
@@ -25,7 +26,7 @@ var fs = require('fs');
 var filesFromMixedPaths = require('./filesFromMixedPaths');
 var extractFromPaths = require('../build/extract').default;
 
-var output = extractFromPaths(argv._);
+var output = extractFromPaths(argv._, { outputFormat: argv.f || 'po' });
 if (argv.o) {
     fs.writeFileSync(argv.o, output);
 } else {
